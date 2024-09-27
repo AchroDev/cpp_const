3200f979e88ac16759d01b4f9e09824b3fbba9d3
AchroDev <achrodev@proton.me>
2024-09-26 21:12:58 -0500

    Changed a to be a const int *

--- main.cpp
+++ main.cpp
@@ -9,11 +9,10 @@ int main()
     const int MAX_AGE = 90;
     const std::string NAME = "AchroDev"; // A const isn't necessarily a "variable", just think of it as a value that will not change
 
-    int *a = new int; // Declaring a new integer variable on the heap
+    const int *a = new int; // Declaring a new integer variable on the heap, now as a const
 
-    *a = 2;              // Since it isn't a const, a can be dereferenced and the value reassigned.
-    a = (int *)&MAX_AGE; // The actual pointer can also be reassigned to something like a reference to MAX_AGE, but you will have to cast the value to a normal int *
-                         // this IS NOT recommended as you are breaking the const promise.
+    *a = 2;              // Since a is now a const int *, you can no longer mutate the data
+    a = (int *)&MAX_AGE; // Although you can't change the data of the pointer, you can still change the pointer itself
     std::cout << *a << std::endl;
 
     std::cin.get();
