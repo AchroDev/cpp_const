3432eaea1c5f6c5f04cad5618d3b1d7369007e39
AchroDev <achrodev@proton.me>
2024-09-26 20:24:08 -0500

    Added const example and comments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+// The const keyword is sort of a fake keyword, mostly because it doesn't do much to change the code
+// It helps with visibility and helps "enforce" rules, i.e. that the data is constant.
+
 int main()
 {
+    int a = 5; // Without signifying that the data is a constant variable
+    a = 2;     // it allows you to change that data later on
 
     std::cin.get();
 }
